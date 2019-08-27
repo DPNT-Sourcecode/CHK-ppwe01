f41bd2a5c55eb154973428a8aca2879c4ae3a1c7
sbx_user1051 <sbx_user1051@169.254.21.85>
2019-08-27 13:16:33 +0000

    Tue Aug 27 13:16:33 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,29 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+	result = [x.strip() for x in skus.split(',')]
+
+	count = 0
+
+	if 'A' in result:
+		a_occurances = result.count("A")
+		if a_occurances >= 3:
+			if a_occurances%3==0:
+				r = a_occurances//3
+				for i in range(r):
+					count+=130
+
+			else:
+				pass
+		else
+			count += 50
+
+
+
+	if 'B' in result:
+		count += 30
+	if 'C' in result:
+		count +=20
+
     raise NotImplementedError()
+
