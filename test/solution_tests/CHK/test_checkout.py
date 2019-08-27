b422d0545a1235c7d1bc0e04540f41914111acd1
sbx_user1051 <sbx_user1051@169.254.21.85>
2019-08-27 13:04:33 +0000

    Tue Aug 27 13:04:33 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,4 +4,7 @@ import unittest
 class TestSum(unittest.TestCase):
     def test_add(self):
         self.assertEqual(checkout_solution.checkout("A"), 50)
+        self.assertEqual(checkout_solution.checkout("A,B"), 80)
+        self.assertEqual(checkout_solution.checkout("A,B,C"), 100)
+
 
